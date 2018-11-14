4ea9595e3efc070aab27a7bc92cfe530139e4ea0
nileshpathade <nilesh.pathade@enyotalearning.com>
2018-11-14 12:14:58 +0530

    add js

--- myjavascript.js
+++ myjavascript.js
@@ -1,4 +1,33 @@
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Showing google Map add Page JS.
+ *
+ * @package    format_classroom
+ * @copyright  2018 eNyota Learning Pvt Ltd.
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
 initAutocomplete();
+
+/**
+ * Calling Google Map Function.
+ * @package    format_classroom
+ * @copyright  2018 eNyota Learning Pvt Ltd.
+ */
 function initAutocomplete() {
     var map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: -33.8688, lng: 151.2195},
